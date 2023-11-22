fd74ef37c4107fd378bddb0615fa98152422dfd1
Muhammed Zain <muhammedzain2256@gmail.com>
2023-11-22 20:15:45 +0500

    Client Side

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+const App = () => {
+  
+  
+  return (
+    <>
+      <nav>Navbar</nav>
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div className="text-primary">Home Page</div>
+  )
+}
+
+export default Home
